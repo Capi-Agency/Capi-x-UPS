4fe8565d3d65085600c029caccbb8eb5c52ad4f0
ducnguyen1002 <133721410+ducnguyen1002@users.noreply.github.com>
2024-10-04 17:44:46 +0700

    1

--- src/components/home/Marquee.tsx
+++ src/components/home/Marquee.tsx
@@ -61,50 +61,54 @@ const ReviewCard = ({
 	body: string;
 }) => {
 	return (
-		<figure
-			className={cn(
-				"group/card relative cursor-pointer overflow-hidden rounded-xl p-[20px] w-[300px] h-[160px] flex flex-col justify-between",
-				// light styles
-				"rounded-[16px] bg-[#161F15] border-[#161F15] hover:bg-conic-gradient",
-				// dark styles
-				"dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
-			)}
-		>
-			<div className="flex flex-row items-center gap-5">
-				<img
-					className="rounded-full"
-					width="60"
-					height="60"
-					alt=""
-					src={img}
-				/>
-				<div className="flex flex-col">
-					<p className="text-base font-medium text-[#8D9595] group-hover/card:text-white">
-						{name} <br />
-						{username}
-					</p>
+		<div className="relative group/card">
+			<figure
+				className={cn(
+					"absolute z-1",
+					" relative cursor-pointer overflow-hidden rounded-xl p-[20px] w-[300px] h-[160px] flex flex-col justify-between",
+					// light styles
+					"rounded-[16px] bg-[#161F15] border-[#161F15] hover:bg-radial-gradient",
+					// dark styles
+					"dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
+				)}
+			>
+				<div className="flex flex-row items-center gap-5">
+					<img
+						className="rounded-full"
+						width="60"
+						height="60"
+						alt=""
+						src={img}
+					/>
+					<div className="flex flex-col">
+						<p className="text-base font-medium text-[#8D9595] group-hover/card:text-white">
+							{name} <br />
+							{username}
+						</p>
+					</div>
 				</div>
-			</div>
-			<blockquote className="mt-2 text-[#075728] group-hover/card:text-[#05BE5A] font-hanken-grotesk text-3xl font-medium leading-normal flex items-start">
-				{body}{" "}
-				<i className="mt-1">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-					>
-						<path
-							fillRule="evenodd"
-							clipRule="evenodd"
-							d="M18.7595 5.98952C18.7595 5.7906 18.6805 5.59984 18.5398 5.45919C18.3991 5.31853 18.2084 5.23952 18.0095 5.23952L9.42519 5.23952C9.01098 5.23952 8.67519 5.5753 8.67519 5.98952C8.67519 6.40373 9.01098 6.73952 9.42519 6.73952L16.1993 6.73952L5.4586 17.4802C5.16571 17.7731 5.16571 18.2479 5.4586 18.5408C5.75149 18.8337 6.22637 18.8337 6.51926 18.5408L17.2595 7.80062V14.5738C17.2595 14.988 17.5953 15.3238 18.0095 15.3238C18.4237 15.3238 18.7595 14.988 18.7595 14.5738L18.7595 5.98952Z"
-							fill="#075728"
-						/>
-					</svg>
-				</i>
-			</blockquote>
-		</figure>
+				<blockquote className="mt-2 text-[#075728] group-hover/card:text-[#05BE5A] font-hanken-grotesk text-3xl font-medium leading-normal flex items-start">
+					{body}{" "}
+					<i className="mt-1">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+						>
+							<path
+								fillRule="evenodd"
+								clipRule="evenodd"
+								d="M18.7595 5.98952C18.7595 5.7906 18.6805 5.59984 18.5398 5.45919C18.3991 5.31853 18.2084 5.23952 18.0095 5.23952L9.42519 5.23952C9.01098 5.23952 8.67519 5.5753 8.67519 5.98952C8.67519 6.40373 9.01098 6.73952 9.42519 6.73952L16.1993 6.73952L5.4586 17.4802C5.16571 17.7731 5.16571 18.2479 5.4586 18.5408C5.75149 18.8337 6.22637 18.8337 6.51926 18.5408L17.2595 7.80062V14.5738C17.2595 14.988 17.5953 15.3238 18.0095 15.3238C18.4237 15.3238 18.7595 14.988 18.7595 14.5738L18.7595 5.98952Z"
+								fill="#075728"
+							/>
+						</svg>
+					</i>
+				</blockquote>
+			</figure>
+			<div className="absolute z-[-1] w-[302px] h-[162px] top-0 left-0 translate-x-[-1px] translate-y-[-1px] rounded-[16px] group-hover/card:bg-border-gradient"></div>
+		</div>
 	);
 };
 
