04102fde6e729ea570cb9bbbde667cc55f1c5765
thanhdv161 <150119460+thanhdv161@users.noreply.github.com>
2025-01-02 13:41:00 +0700

    fix btn header

--- src/components/common/the-header/page.tsx
+++ src/components/common/the-header/page.tsx
@@ -57,23 +57,33 @@ const TheHeader = ({ isOpen, setIsOpen }: TheHeaderProps) => {
       case '/nen-tang-cong-nghe':
         setBgColor('bg-[#000]');
         setbgBtn('#02E56A');
+        setcolorText('#15171E');
+        setFillIcon('#014227');
         break;
       case '/khoa-hoc':
         setBgColor('bg-[#1B0C01]');
         setbgBtn('#02E56A');
+        setcolorText('#15171E');
+        setFillIcon('#014227');
         break;
 
       case '/tu-van':
         setBgColor('bg-[#080F25]');
         setbgBtn('#02E56A');
+        setcolorText('#15171E');
+        setFillIcon('#014227');
         break;
       case '/co-phieu':
         setBgColor('border-b-[1px] border-[#D0F500] bg-black');
         setbgBtn('#D0F500');
+        setcolorText('#15171E');
+        setFillIcon('#014227');
         break;
       case '/bieu-phi':
         setBgColor('border-b-[1px] border-[#D0F500] bg-black');
         setbgBtn('#D0F500');
+        setcolorText('#15171E');
+        setFillIcon('#014227');
         break;
       default:
         setBgColor('bg-[#0F1B0E]');
@@ -679,8 +689,8 @@ const TheHeader = ({ isOpen, setIsOpen }: TheHeaderProps) => {
                         fill="none"
                       >
                         <path
-                          fill-rule="evenodd"
-                          clip-rule="evenodd"
+                          fillRule="evenodd"
+                          clipRule="evenodd"
                           d="M18.7589 5.98952C18.7589 5.7906 18.6799 5.59984 18.5393 5.45919C18.3986 5.31853 18.2078 5.23952 18.0089 5.23952L9.42466 5.23952C9.01045 5.23952 8.67466 5.5753 8.67466 5.98952C8.67466 6.40373 9.01045 6.73952 9.42466 6.73952L16.1987 6.73952L5.45807 17.4802C5.16517 17.7731 5.16517 18.2479 5.45807 18.5408C5.75096 18.8337 6.22583 18.8337 6.51873 18.5408L17.2589 7.80062V14.5738C17.2589 14.988 17.5947 15.3238 18.0089 15.3238C18.4231 15.3238 18.7589 14.988 18.7589 14.5738L18.7589 5.98952Z"
                           fill={`${fillIcon}`}
                         />
@@ -709,8 +719,8 @@ const TheHeader = ({ isOpen, setIsOpen }: TheHeaderProps) => {
                         fill="none"
                       >
                         <path
-                          fill-rule="evenodd"
-                          clip-rule="evenodd"
+                          fillRule="evenodd"
+                          clipRule="evenodd"
                           d="M18.7589 5.98952C18.7589 5.7906 18.6799 5.59984 18.5393 5.45919C18.3986 5.31853 18.2078 5.23952 18.0089 5.23952L9.42466 5.23952C9.01045 5.23952 8.67466 5.5753 8.67466 5.98952C8.67466 6.40373 9.01045 6.73952 9.42466 6.73952L16.1987 6.73952L5.45807 17.4802C5.16517 17.7731 5.16517 18.2479 5.45807 18.5408C5.75096 18.8337 6.22583 18.8337 6.51873 18.5408L17.2589 7.80062V14.5738C17.2589 14.988 17.5947 15.3238 18.0089 15.3238C18.4231 15.3238 18.7589 14.988 18.7589 14.5738L18.7589 5.98952Z"
                           fill={`${fillIcon}`}
                         />
